fad04dde4cbc7fa929513ec949abd97582b7bcd0
Repertus <robertmdelc@gmail.com>
2016-08-25 10:29:05 -0700

    Second Commit

--- app/app.js
+++ app/app.js
@@ -1,14 +1,6 @@
-angular.module('app', []);
+(function() {
+    'use strict';
 
-
-
-// 
-// Code for running without a factory
-// 
-
-// angular.module('myApp', [])
-// 	.controller('hotspotsCtrl', function($scope, $http) {
-// 		$http.get('../topspots.json').then(function(response) {
-// 			$scope.hotspots = response.data;
-// 		});
-// 	});
+    angular
+        .module('app', []);
+})();
--- app/topSpots.factory.js
+++ app/topSpots.factory.js
@@ -1,8 +1,37 @@
-angular.module('app').factory('topspotsFactory', function($http){
-	return {
-		getTopSpots: function () {
-			// return $http.get('../topspots.json');
-			return $http.get('http://localhost:52160/api/TopSpots');
-		}
-	};
-})
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('topspotsFactory', topspotsFactory);
+
+    topspotsFactory.$inject = ['$http', '$q'];
+
+    /* @ngInject */
+    function topspotsFactory($http, $q) {
+        var service = {
+            getTopSpots: getTopSpots
+        };
+        return service;
+
+        ////////////////
+
+        function getTopSpots() {
+        	var defer = $q.defer();
+            $http.get('http://localhost:52160/api/TopSpots').then(
+            	function(response) {
+                   if (typeof response.data === 'object') {
+                       defer.resolve(response.data);
+                   } else {
+                       defer.reject(response.data);
+                   }
+               },
+               function(error) {
+                   defer.reject(error);
+               }
+           );
+
+           return defer.promise;
+        }
+    }
+})();
